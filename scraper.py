903949df354eb847045dfe9fe9888ababf50cec4
Todd Qualls <tqualls@gmail.com>
2015-08-20 20:57:39 -0400

    Fixes unicode conversion back to html! Also, comments out Rob Connery links until I figure out how to interpret his new DOM.

--- scraper.py
+++ scraper.py
@@ -13,14 +13,14 @@ def writeFile(links, num_of_articles, file_name, title):
 	list_open = '<ol>'
 	list_close = '</ol>'
 	doc_end = '</div>'
-	document = open(file_name, 'w', encoding='windows-1252')
+	document = open(file_name, 'w', encoding='ascii')
 	document.truncate()
 	document.write(doc_start)
 	document.write(heading)
 	document.write(list_open)
 	for i in range(0, num_of_articles):
 		print (str(links[i]).encode('utf8'))
-		document.write('<li>'+str(links[i])+'</li>')
+		document.write('<li>'+str(links[i].prettify(formatter="html"))+'</li>')
 	document.write(list_close)
 	document.write(doc_end)
 	document.close()
@@ -61,19 +61,34 @@ def createConeryLinks():
 
 	soup = makeSoup(url)
 
-	articles = soup.findAll('article',attrs={'class':'post'})
+	post_listing = soup.findAll('section', attrs={'class':'post-listing'})
+
+	articles = post_listing[0].findAll('article',attrs={'class':'post'})
 
 	article_soup = BeautifulSoup(str(articles), "html.parser")
 
-	headings = article_soup.findAll('h2')
+	post_contents = soup.findAll('div',attrs={'class':'post-content'})
+
+	post_contents_soup = BeautifulSoup(str(post_contents), "html.parser")
+
+	headings = post_contents_soup.findAll('h2')
 
 	heading_soup = BeautifulSoup(str(headings), "html.parser")
 
 	links = heading_soup.findAll('a')
 
-	writeFile(links, 10, 'rc.html', "Rob Conery's Last 10 Posts")
+	# for li in links:
+	# 	print(str(li))
+
+	# for heading in headings:
+	# 	print(str(heading))
+
+	# for pl in post_listing:
+	# 	print(str(pl.prettify('utf8')))
+
+	# writeFile(links, 10, 'rc.html', "Rob Conery's Last 10 Posts")
 
 
 createHackerNewsLinks()
 createHanselmanLinks()
-createConeryLinks()
+# createConeryLinks()
